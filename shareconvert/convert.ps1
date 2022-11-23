37cb33f37b437a82eb18591c8f9f8ecf0a660341
Kory <kalbert1991@gmail.com>
2022-11-23 10:04:10 -0800

    Create convert.ps1

---
+++ shareconvert/convert.ps1
@@ -0,0 +1,21 @@
+#Who is doing this?
+$Credentials = Get-Credential
+
+#Set vars
+$shareduser=$env:COMPUTERNAME
+
+#Set Regedit path
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+
+#Delete shared keys for autologin
+Remove-ItemProperty -Path $RegPath -Name "AutoAdminLogon"
+Remove-ItemProperty -Path $RegPath -Name "DefaultUsername"
+Remove-ItemProperty -Path $RegPath -Name "DefaultPassword"
+Remove-ItemProperty -Path $RegPath -Name "ForceAutoLogon"
+
+#Remove user from AD
+#Remove-ADUser -Identity $shareduser
+
+#Log to origianl build file
+"Share to Uni convert" |Out-File C:\Build\Build.txt -Append
+("Configured by " + $Credentials.UserName + " on: " + (Get-Date).ToString()) | Out-File C:\Build\Build.txt -Append
