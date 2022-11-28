08efc65cf5e9115f4be87e993dfaf7b351260f2e
Kory <kalbert1991@gmail.com>
2022-11-28 09:30:27 -0800

    Update run.bat

--- shareconvert/run.bat
+++ shareconvert/run.bat
@@ -1,5 +1,6 @@
 echo off
 
 
-MsiExec.exe /I{747840DC-0E27-476E-8279-34A8CC5DF3C0}
+MsiExec.exe /I{747840DC-0E27-476E-8279-34A8CC5DF3C0} /qn
+MsiExec.exe /X{562B7F33-EA0E-4C2E-A270-23CAA72C1480} /qn
 start /D C:\ powershell -ExecutionPolicy Bypass C:\sharedconvert\shareconvert.ps1cmd
