d531772768265d2e1b1c51b010e69a0ee15573b3
Kory <kalbert1991@gmail.com>
2022-11-28 13:08:20 -0800

    Update convert.ps1

--- shareconvert/convert.ps1
+++ shareconvert/convert.ps1
@@ -2,7 +2,8 @@
 $Credentials = Get-Credential
 
 #Set Pre requirments
-Add-WindowsCapability -Online -Name "RSAT: Active Directory Domain Services and Lightweight Directory Services Tools"
+Add-WindowsCapability -Online -Name "Rsat.ServerManager.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
 
 #Set vars
 $shareduser=$env:COMPUTERNAME
@@ -22,22 +23,17 @@ Remove-ItemProperty -Path $RegPath -Name "DefaultPassword"
 Remove-ItemProperty -Path $RegPath -Name "ForceAutoLogon"
 
 #Remove user from AD
-Remove-ADUser -Identity $shareduser
+Remove-ADUser -Identity $shareduser -Confirm:$False
 
 #Delete AD memberships
 Remove-ADGroupMember -Identity "Shared" -Members $ComputerDN -Confirm:$False
 Remove-ADGroupMember -Identity "App-Imprivata Shared (Logout)" -Members $ComputerDN -Confirm:$False
 
-#Remove Shared version of Imprivada
-#Start-Process -FilePath “MsiExec.exe" -ArgumentList "/X{562B7F33-EA0E-4C2E-A270-23CAA72C1480} /qn"
-
 #Delete local shared user account
 Remove-Item -LiteralPath "C:\Users\$shareduser" -Force -Recurse
 
-
-
 #Reinstall imprivada as unique
-#msiexec /i “ImprivataAgent_x64.msi" IPTXPRIMSERVER="HTTPS://TFHD-IMPRIV01.TFHD.AD/sso/servlet/messagerouter" AGENTTYPE=1 /qn /norestart
+#msiexec.exe /i “ImprivataAgent_x64.msi" IPTXPRIMSERVER="HTTPS://TFHD-IMPRIV01.TFHD.AD/sso/servlet/messagerouter" AGENTTYPE=1 /qn /norestart
 
 #Add new AD memberships
 $App = @(
@@ -48,38 +44,38 @@ Start-Job -Credential $Credentials -ArgumentList $App -ScriptBlock {
     Param($App)
 
     [int] $ADS_PROPERTY_APPEND = 3
-	#Get the computer DN
-	$SysInfo = New-Object -ComObject "ADSystemInfo"
-	$UserDN = $SysInfo.GetType().InvokeMember("ComputerName", "GetProperty", $Null, $SysInfo, $Null)
-	$ComputerDN = "LDAP://$UserDN"
-	#Get the Domain DN
-	$ORoot = [ADSI]"LDAP://rootDSE"
-	$strDomainPath = $ORoot.Get("defaultNamingContext")
-	#Create ADODB connection
-	$oConnection = New-Object -ComObject "ADODB.Connection"
-	$oConnection.Provider= "ADsDSOObject"
-	$oConnection.Open("Active Directory Provider")
+    #Get the computer DN
+    $SysInfo = New-Object -ComObject "ADSystemInfo"
+    $UserDN = $SysInfo.GetType().InvokeMember("ComputerName", "GetProperty", $Null, $SysInfo, $Null)
+    $ComputerDN = "LDAP://$UserDN"
+    #Get the Domain DN
+    $ORoot = [ADSI]"LDAP://rootDSE"
+    $strDomainPath = $ORoot.Get("defaultNamingContext")
+    #Create ADODB connection
+    $oConnection = New-Object -ComObject "ADODB.Connection"
+    $oConnection.Provider= "ADsDSOObject"
+    $oConnection.Open("Active Directory Provider")
 
     foreach($App in $App)
     {
         #Get the specefied group
-		$oRs = $oConnection.Execute("SELECT adspath FROM 'LDAP://$strDomainPath' WHERE objectCategory='group' AND  Name='$App'")
-		If (!$oRs.EOF)
-		{
-			$strAdsPath = ($oRs.Fields |  Select value ).value
-		}
-		If($strAdsPath)
-		{
-			$objGroup = [ADSI]$strAdsPath
-			$objComputer = [ADSI]$ComputerDN
-			#verify if the computer is a member of the Group
-			If ($objGroup.ismember($objComputer.adspath) -eq $false) 
-			{
-				#Add the the computer to the specefied group
-				$objGroup.PutEx($ADS_PROPERTY_APPEND,"member",@("$UserDN"))
-				$objGroup.setinfo()
-			}
-		}
+        $oRs = $oConnection.Execute("SELECT adspath FROM 'LDAP://$strDomainPath' WHERE objectCategory='group' AND  Name='$App'")
+        If (!$oRs.EOF)
+        {
+            $strAdsPath = ($oRs.Fields |  Select value ).value
+        }
+        If($strAdsPath)
+        {
+            $objGroup = [ADSI]$strAdsPath
+            $objComputer = [ADSI]$ComputerDN
+            #verify if the computer is a member of the Group
+            If ($objGroup.ismember($objComputer.adspath) -eq $false) 
+            {
+                #Add the the computer to the specefied group
+                $objGroup.PutEx($ADS_PROPERTY_APPEND,"member",@("$UserDN"))
+                $objGroup.setinfo()
+            }
+        }
     }
 }
 Start-Sleep -s 10
@@ -91,38 +87,38 @@ Start-Job -Credential $Credentials -ArgumentList $App -ScriptBlock {
     Param($App)
 
     [int] $ADS_PROPERTY_APPEND = 3
-	#Get the computer DN
-	$SysInfo = New-Object -ComObject "ADSystemInfo"
-	$UserDN = $SysInfo.GetType().InvokeMember("ComputerName", "GetProperty", $Null, $SysInfo, $Null)
-	$ComputerDN = "LDAP://$UserDN"
-	#Get the Domain DN
-	$ORoot = [ADSI]"LDAP://rootDSE"
-	$strDomainPath = $ORoot.Get("defaultNamingContext")
-	#Create ADODB connection
-	$oConnection = New-Object -ComObject "ADODB.Connection"
-	$oConnection.Provider= "ADsDSOObject"
-	$oConnection.Open("Active Directory Provider")
+    #Get the computer DN
+    $SysInfo = New-Object -ComObject "ADSystemInfo"
+    $UserDN = $SysInfo.GetType().InvokeMember("ComputerName", "GetProperty", $Null, $SysInfo, $Null)
+    $ComputerDN = "LDAP://$UserDN"
+    #Get the Domain DN
+    $ORoot = [ADSI]"LDAP://rootDSE"
+    $strDomainPath = $ORoot.Get("defaultNamingContext")
+    #Create ADODB connection
+    $oConnection = New-Object -ComObject "ADODB.Connection"
+    $oConnection.Provider= "ADsDSOObject"
+    $oConnection.Open("Active Directory Provider")
 
     foreach($App in $App)
     {
         #Get the specefied group
-		$oRs = $oConnection.Execute("SELECT adspath FROM 'LDAP://$strDomainPath' WHERE objectCategory='group' AND  Name='$App'")
-		If (!$oRs.EOF)
-		{
-			$strAdsPath = ($oRs.Fields |  Select value ).value
-		}
-		If($strAdsPath)
-		{
-			$objGroup = [ADSI]$strAdsPath
-			$objComputer = [ADSI]$ComputerDN
-			#verify if the computer is a member of the Group
-			If ($objGroup.ismember($objComputer.adspath) -eq $false) 
-			{
-				#Add the the computer to the specefied group
-				$objGroup.PutEx($ADS_PROPERTY_APPEND,"member",@("$UserDN"))
-				$objGroup.setinfo()
-			}
-		}
+        $oRs = $oConnection.Execute("SELECT adspath FROM 'LDAP://$strDomainPath' WHERE objectCategory='group' AND  Name='$App'")
+        If (!$oRs.EOF)
+        {
+            $strAdsPath = ($oRs.Fields |  Select value ).value
+        }
+        If($strAdsPath)
+        {
+            $objGroup = [ADSI]$strAdsPath
+            $objComputer = [ADSI]$ComputerDN
+            #verify if the computer is a member of the Group
+            If ($objGroup.ismember($objComputer.adspath) -eq $false) 
+            {
+                #Add the the computer to the specefied group
+                $objGroup.PutEx($ADS_PROPERTY_APPEND,"member",@("$UserDN"))
+                $objGroup.setinfo()
+            }
+        }
     }
 }
 
@@ -131,4 +127,9 @@ Start-Job -Credential $Credentials -ArgumentList $App -ScriptBlock {
 ("Configured by " + $Credentials.UserName + " on: " + (Get-Date).ToString()) | Out-File C:\Build\Build.txt -Append
 
 Start-Sleep -s 10
+
+#Remove extra admin tools
+Remove-WindowsCapability -Online -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
+Remove-WindowsCapability -Online -Name "Rsat.ServerManager.Tools~~~~0.0.1.0"
+
 restart-computer
