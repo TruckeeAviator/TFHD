0f5f13a1001bf605da595123850ef6bf75109b2a
Kory <kalbert1991@gmail.com>
2022-11-29 07:31:14 -0800

    Update run.bat

--- shareconvert/run.bat
+++ shareconvert/run.bat
@@ -1,12 +1,3 @@
 echo off
 cls
-
-echo **Removing ImprivataAgent Shared**
-MsiExec.exe /I{747840DC-0E27-476E-8279-34A8CC5DF3C0} /qn
-MsiExec.exe /X{562B7F33-EA0E-4C2E-A270-23CAA72C1480} /qn REBOOT=ReallySuppress /norestart
-echo **Installing ImprivataAgent Unique**
-cd "C:\sharedconvert"
-msiexec.exe /i “ImprivataAgent_x64.msi" IPTXPRIMSERVER="HTTPS://TFHD-IMPRIV01.TFHD.AD/sso/servlet/messagerouter" AGENTTYPE=1 /qn /norestart
-echo **Wait for ImprivataAgent to install**
-timeout /t 120
 start /D C:\ powershell -ExecutionPolicy Bypass C:\sharedconvert\convert.ps1
