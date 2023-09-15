3d776aea935fa04a47d572280340562bea38bd79
Kory <kalbert1991@gmail.com>
2023-09-15 09:19:26 -0700

    Added ping check
    Script would fail at copy command if the host was down. added logic to test if host was up. If not continue to next and log.

--- AdobeRemoveUpdate/update.py
+++ AdobeRemoveUpdate/update.py
@@ -1,6 +1,18 @@
 import os
 import shutil
 from datetime import datetime
+import subprocess
+import platform 
+
+def ping(host):
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    command = ['ping', param, '1', host]
+
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf8')
+    if "Destination host unreachable" in output or "100% packet loss" in output:
+        return 1
+    return 0
 
 #convert string into a list
 def convertString(string):
@@ -23,12 +35,20 @@ data = convertString(data)
 #Loop through the list
 for x in data:
     print("Running on: " + x.upper())
+
+    if ping(x) != 0:
+        print("Host " + x.upper() + " is off, skipping")
+        outputFile = open("outputLog.txt", "a")
+        outputFile.write(datetime.now().strftime("%D %H:%M") + ": " "Skipped " + x.upper() + "\n")
+        outputFile.close()
+        continue
+
     removeUpdate(x)
-    print("*** Finished install on: " + x.upper())
+    print("*** Finished install on: " + x.upper() + " ***")
 
     #Write to log file
     outputFile = open("outputLog.txt", "a")
-    outputFile.write(datetime.now().strftime("%D %H:%M") + ": " "Installed on " + x.upper())
+    outputFile.write(datetime.now().strftime("%D %H:%M") + ": " "Installed on " + x.upper() + "\n")
     outputFile.close()
 
 file.close()
